2d65532d97b14ce10515a1511e278a584146b8fd
franciscoduc4 <fduca@fi.uba.ar>
2024-05-11 19:07:18 -0300

    added groups table expansion

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,121 +17,122 @@ const Home = () => {
     const navigate = useNavigate();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [groupName, setGroupName] = useState('');
-    // Usar el tipo definido al declarar el estado
     const [groups, setGroups] = useState<Group[]>([]);
+    const [showGroups, setShowGroups] = useState(false);  // Estado para mostrar/ocultar la tabla de grupos
 
     const handleOpenModal = () => setIsModalOpen(true);
-    const handleCloseModal = () => setIsModalOpen(false);
-    const handleGroupNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setGroupName(e.target.value);
-    const handleCreateGroup = () => {
-        const newGroup = {
-            name: groupName,
-            members: ["Creador (Ejemplo)"],
-            memberCount: 1
-        };
-        setGroups([...groups, newGroup]);
+    const handleCloseModal = () => {
         setIsModalOpen(false);
-        setGroupName('');
+        setGroupName('');  // Limpiar el campo después de cerrar el modal
+    };
+    const handleGroupNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setGroupName(e.target.value);
+    
+    const handleCreateGroup = () => {
+        if (groupName.trim() !== '') {
+            const newGroup = {
+                name: groupName,
+                members: ["Creador (Ejemplo)"],
+                memberCount: 1
+            };
+            setGroups([...groups, newGroup]);  // Agregar el nuevo grupo al estado
+            handleCloseModal();  // Cerrar el modal y limpiar el formulario
+        }
+    };
+
+    const toggleGroups = () => {
+        setShowGroups(!showGroups);  // Cambiar estado para mostrar/ocultar grupos
     };
 
     function handleSignOut() {
         auth.signOut().then(() => {
-            // Sign-out successful.
             console.log("Sesión cerrada exitosamente.");
             navigate('/billy/login');
         }).catch((error) => {
-            // An error happened.
             console.error("Error al cerrar sesión:", error);
         });
-        console.log('Cerrar Sesión');
     }
     useEffect(() => {
         const user = auth.currentUser;
         if (!user) {
             navigate('/billy/login');
         }
-        //console.log('Usuario:', user.email);
-    },[]);
+    }, []);
 
     return (
         <CssVarsProvider>
             <CssBaseline />
-            <Box sx={{ p: 4 }}>
-                <Button onClick={handleOpenModal} sx={{ mb: 2 }}>
-                    Crear nuevo grupo
-                </Button>
-                <Button onClick={handleSignOut} sx={{ mb: 2 }}>
-                    Cerrar Sesión
-                </Button>
+            <Box sx={{ display: 'flex', justifyContent: 'center', p: 4, alignItems: 'flex-start' }}>
+                <Box sx={{ width: '50%' }}>
+                    <Button onClick={handleOpenModal} sx={{ mb: 2 }}>
+                        Crear nuevo grupo
+                    </Button>
+                    {/* Botón "Mis grupos" centrado */}
+                    <Box sx={{ justifyContent: 'center', textAlign: 'center', mt: 4 }}>
+                        <Button onClick={toggleGroups} sx={{}}>
+                            <Typography level="h4">Grupos</Typography>
+                        </Button>
+                    </Box>
+                    {showGroups && (
+                        <Box sx={{ overflowY: 'auto' }}>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre del Grupo</th>
+                                        <th>Administrador</th>
+                                        <th>Cantidad de miembros</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {groups.map((group, index) => (
+                                        <tr key={index}>
+                                            <td>
+                                                <Link to={`/billy/group/${group.name}`} className="link">
+                                                    {group.name}
+                                                </Link>
+                                            </td>
+                                            <td>{group.members.join(', ')}</td>
+                                            <td>{group.memberCount}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </Box>
+                    )}
+                </Box>
+
+                <Box>
+                    <Button onClick={handleSignOut} sx={{ mb: 2 }}>
+                        Cerrar Sesión
+                    </Button>
+                </Box>
 
                 {isModalOpen && (
                     <Box sx={{ position: 'fixed', inset: 0, display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
                         <Box sx={{
-                            bgcolor: 'black',  // Cambiar el color de fondo a negro
-                            color: 'white',  // Cambiar el color del texto a blanco para contraste
+                            bgcolor: 'background.paper',
                             p: 3,
-                            pt: 2,
                             borderRadius: '16px',
                             boxShadow: 6,
                             minWidth: '300px',
-                            border: '1px solid #bdbdbd',
+                            border: '1px solid grey'  // Agregar borde gris alrededor del modal
                         }}>
-                            <Typography level="h3" sx={{ mb: 1, color: 'white' }}>Nuevo grupo</Typography>
+                            <Typography level="h3" sx={{ mb: 1 }}>Nuevo grupo</Typography>
                             <input
                                 type="text"
                                 placeholder="Nombre del grupo"
                                 value={groupName}
                                 onChange={handleGroupNameChange}
-                                className="input"
-                                style={{
-                                    display: 'block',
-                                    width: '100%',
-                                    marginBottom: '16px',
-                                    padding: '8px',
-                                    fontSize: '16px',
-                                    border: '1px solid #bdbdbd',
-                                    borderRadius: '16px',
-                                    backgroundColor: '#333',  // Fondo del input para mejorar visibilidad
-                                    color: 'white'  // Color del texto del input
-                                }}
+                                style={{ width: '100%', marginBottom: '16px', padding: '8px', border: '1px solid #bdbdbd', borderRadius: '16px' }}
                             />
-                            <Box sx={{ display: 'flex', justifyContent: 'flex-start', gap: 1 }}>
-                                <Button onClick={handleCloseModal} sx={{ color: 'white' }}>
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Button onClick={handleCloseModal}>
                                     Cancelar
                                 </Button>
-                                <Button onClick={handleCreateGroup} sx={{ color: 'white' }}>
+                                <Button onClick={handleCreateGroup}>
                                     Crear
                                 </Button>
                             </Box>
                         </Box>
-                    </Box>
-
-                )}
-
-                {groups.length > 0 && (
-                    <Box sx={{ mt: 4 }}>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Nombre del Grupo</th>
-                                    <th>Administrador</th>
-                                    <th>Cantidad de miembros</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {groups.map((group, index) => (
-                                    <tr key={index}>
-                                        <td>
-                                            <Link to={`/billy/group/${group.name}`} className="link">
-                                                {group.name}
-                                            </Link>
-                                        </td>
-                                        <td>{group.members.join(', ')}</td>
-                                        <td>{group.memberCount}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
                     </Box>
                 )}
             </Box>
